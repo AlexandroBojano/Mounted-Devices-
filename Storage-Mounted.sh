144f28314598d76dd2315f21df113fea2d121261
AlexandroBojano <122400617+AlexandroBojano@users.noreply.github.com>
2024-07-01 09:09:36 -0300

    Script Mounted Storage

---
+++ Storage-Mounted.sh
@@ -0,0 +1,9 @@
+#!/usr/bin/env bash
+
+## Dados ##
+
+sudo mount UUID="4d4e7142-55cb-41b9-9831-2998d282007b"  /media/lacroy/Dados         
+
+sudo mount UUID="9e19d89e-77e7-49d5-9d2f-922106ecb91a" /media/lacroy/Developer 
+
+
